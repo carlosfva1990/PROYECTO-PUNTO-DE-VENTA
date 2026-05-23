e46b507251a0da52785b0e938824ebb371ce5221
carlosfva1990 <carlosfva_1990@hotmail.com>
2026-05-22 20:35:43 -0600

    modificaciones para que compile

--- demo/src/main/java/com/tienda/Main.java
+++ demo/src/main/java/com/tienda/Main.java
@@ -1,7 +1,11 @@
 package com.tienda;
 
+import org.springframework.boot.SpringApplication;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        SpringApplication.run(TiendaOnlineApplication.class, args);
     }
 }
